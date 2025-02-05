9ae6d8022d4084d4178afd2e190e846145e3b229
mekkyz <mos.mekky@gmail.com>
2025-02-05 01:36:29 +0100

    refactor: extracting ui logic from sub hero sections

--- src/components/home/LabimotionHero.js
+++ src/components/home/LabimotionHero.js
@@ -1,72 +1,25 @@
 'use client';
 
-import { motion } from 'framer-motion';
-import useSWR from 'swr';
-import fallback from '/public/fallback.json';
+import useContent from '@/hooks/useContent';
+import SubHero from '@/components/ui/SubHero';
 
-const fetcher = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/labimotion-section?populate=*`, {
-    headers: {
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`
-    }
+export default function LabimotionHero() {
+  const { content, isLoading } = useContent({
+    apiKey: 'labimotion-section',
+    fallbackKey: 'labimotionSection'
   });
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
-  return res.json();
-};
-
-const LabimotionHero = () => {
-  const { data, error, isLoading } = useSWR('labimotion-section', fetcher);
-
-  const fallbackContent = fallback.labimotionSection;
-  if (isLoading) return <div>Loading...</div>;
-
-  const content = !error && data?.data ? data.data : fallbackContent;
-
-  const heroTitle = content?.heroTitle || [];
-  const hoverEffect = {
-    y: -5,
-    cursor: 'default'
-  };
-
   return (
-    <motion.div
-      id="labimotion"
-      className="relative my-10 flex cursor-default scroll-mt-[110px] flex-col items-center text-[#e63946]"
-      whileHover="hover"
-      role="region"
-      aria-labelledby="labimotion-hero-heading">
-      <h1
-        id="labimotion-hero-heading"
-        className="text-center text-4xl"
-        aria-label="Out of Chemistry. Generic Elements for Life Sciences">
-        {heroTitle.map((paragraph, idx) => (
-          <span key={idx}>
-            {paragraph.children.map((child, childIdx) =>
-              child.bold ? (
-                <motion.span
-                  key={childIdx}
-                  className="inline-block font-bold"
-                  variants={{
-                    hover: hoverEffect
-                  }}
-                  transition={{
-                    type: 'spring',
-                    stiffness: 500
-                  }}>
-                  {child.text}
-                </motion.span>
-              ) : (
-                <span key={childIdx}>{child.text}</span>
-              )
-            )}
-          </span>
-        ))}
-      </h1>
-    </motion.div>
+    <SubHero
+      heroId="labimotion"
+      headingId="labimotion-hero-heading"
+      headingAriaLabel="Out of Chemistry. Generic Elements for Life Sciences"
+      containerAriaLabelledby="labimotion-hero-heading"
+      textColorClass="text-[#e63946]"
+      containerClassName="relative my-10 flex cursor-default scroll-mt-[110px] flex-col items-center"
+      titleClassName="text-center text-4xl"
+      content={content}
+      isLoading={isLoading}
+    />
   );
-};
-
-export default LabimotionHero;
+}
---
+++ src/components/ui/SubHero.js
@@ -0,0 +1,60 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+export default function SubHero({
+  heroId,
+  headingId,
+  headingAriaLabel,
+  containerAriaLabelledby,
+  textColorClass = 'text-[#008ab8]',
+  containerClassName = 'relative my-10 flex cursor-default scroll-mt-[110px] flex-col items-center',
+  titleClassName = 'text-center text-4xl',
+
+  content,
+  isLoading
+}) {
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!content) {
+    return <div>No content available</div>;
+  }
+
+  const heroTitle = content?.heroTitle || [];
+
+  const hoverEffect = {
+    y: -5,
+    cursor: 'default'
+  };
+
+  return (
+    <motion.div
+      id={heroId}
+      className={`${textColorClass} ${containerClassName}`}
+      whileHover="hover"
+      role="region"
+      aria-labelledby={containerAriaLabelledby}>
+      <h1 id={headingId} className={titleClassName} aria-label={headingAriaLabel}>
+        {heroTitle.map((paragraph, idx) => (
+          <span key={idx}>
+            {paragraph.children.map((child, childIdx) =>
+              child.bold ? (
+                <motion.span
+                  key={childIdx}
+                  className="inline-block font-bold"
+                  variants={{ hover: hoverEffect }}
+                  transition={{ type: 'spring', stiffness: 500 }}>
+                  {child.text}
+                </motion.span>
+              ) : (
+                <span key={childIdx}>{child.text}</span>
+              )
+            )}
+          </span>
+        ))}
+      </h1>
+    </motion.div>
+  );
+}
