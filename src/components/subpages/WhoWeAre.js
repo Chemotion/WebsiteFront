e43090eb1c8a35a72b00286464d2f5df90e8a814
mekkyz <mos.mekky@gmail.com>
2025-03-10 17:08:32 +0100

    remove: console test logging

--- src/components/subpages/WhoWeAre.js
+++ src/components/subpages/WhoWeAre.js
@@ -9,7 +9,6 @@ export default function WhoWeArePage() {
     apiKey: 'about-page',
     fallbackKey: 'aboutPage'
   });
-  console.log('Content:', content);
 
   if (isLoading) return <div>Loading...</div>;
 
