9439997a243b16561a115ce620cc0cd90244fa47
mekkyz <mos.mekky@gmail.com>
2025-03-10 13:17:32 +0100

    feat: adapt not found page to strapi

--- src/components/subpages/NotFound.js
+++ src/components/subpages/NotFound.js
@@ -1,32 +1,45 @@
+'use client';
+
 import Image from 'next/image';
 import { HeroButton } from '@/components/ui/HeroButton';
+import useContent from '@/hooks/useContent';
 
 export default function NotFoundPage() {
+  const { content, isLoading } = useContent({
+    apiKey: 'not-found-page',
+    fallbackKey: 'notFoundPage'
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+
+  const { mainText, subText, buttonText, buttonLink, logo } = content;
+
   return (
     <div className="mx-auto mb-20 mt-10 max-w-4xl p-4 text-center" role="main" aria-labelledby="not-found-title">
       <div className="mb-12">
         <Image
-          src="/images/chemotion-lg.png"
-          alt="Illustration of Chemotion Logo for Not Found Page"
+          src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${logo?.url}`}
+          alt={logo?.alternativeText}
           width={500}
           height={301}
+          unoptimized
           className="mx-auto"
         />
       </div>
       <section className="mb-12">
-        <h1 id="not-found-title" className="text-5xl font-extrabold leading-normal" aria-label="404 - Page Not Found">
-          404 - PAGE NOT FOUND
+        <h1 id="not-found-title" className="text-5xl font-extrabold leading-normal" aria-label={mainText}>
+          {mainText}
         </h1>
         <p className="mt-4 text-lg" aria-label="Explanation that the page is not found">
-          Sorry, the page you are looking for does not exist. It might have been moved, renamed, or removed.
+          {subText}
         </p>
       </section>
       <HeroButton
         as="a"
-        href="/"
+        href={buttonLink}
         aria-label="Go back to home page"
         className="w-48 border-2 border-[#008ab8] bg-[#008ab8] font-semibold dark:border-darkForeground dark:bg-darkBackground">
-        <div className="py-2 text-xl text-white"> Back to home</div>
+        <div className="py-2 text-xl text-white">{buttonText}</div>
       </HeroButton>
     </div>
   );
